a968ecd016d6c9c11d9d8d03bde66bb0e0873b15
KingS <770234358@qq.com>
2021-05-30 02:54:23 +0800

    spring5源码环境搭建

---
+++ spring-mytest/src/main/java/com/wq/Test.java
@@ -0,0 +1,16 @@
+package com.wq;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+
+		System.out.println("==================");
+		System.out.println("hah");
+		AnnotationConfigApplicationContext context = new
+				AnnotationConfigApplicationContext(JavaConfig.class);
+		User user = (User) context.getBean("user");
+		System.out.println(user.toString());
+		System.out.println("==================");
+	}
+}
