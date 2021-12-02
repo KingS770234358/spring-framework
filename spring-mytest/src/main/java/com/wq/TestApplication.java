0f2728e272e6854c306ae5f3deb82f12abb72bc8
KingS <770234358@qq.com>
2021-12-02 13:54:34 +0800

    Spring5 源码构建 配置修改

--- spring-mytest/src/main/java/com/wq/TestApplication.java
+++ spring-mytest/src/main/java/com/wq/TestApplication.java
@@ -7,11 +7,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestApplication {
 	public static void main(String[] args) {
 
-		System.out.println("==================");
-		AnnotationConfigApplicationContext context = new
-				AnnotationConfigApplicationContext(TestConfig.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
 		User user = (User) context.getBean("user");
-		System.out.println(user.toString());
-		System.out.println("==================");
+		System.out.println(user);
 	}
 }
